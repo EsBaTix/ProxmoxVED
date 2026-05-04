e34922a9471bb87b23d0ca66903c7a7b5d1721f1
EsBaTix <esbatix+github@posteo.net>
2026-05-04 13:50:37 +0200

    using correct path for update

--- ct/anki-sync-server.sh
+++ ct/anki-sync-server.sh
@@ -23,7 +23,7 @@ function update_script() {
   header_info
   check_container_storage
   check_container_resources
-  if [[ ! -f /lib/systemd/system/anki-sync-server.service ]]; then
+  if [[ ! -f /etc/systemd/system/anki-sync-server.service ]]; then
     msg_error "No ${APP} Installation Found!"
     exit
   fi
